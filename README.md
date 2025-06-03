152b910708bde4b0036e585e1e3725a4eadb95fd
Olivier Marteaux <olivier_marteaux@hotmail.com>
2025-06-03 10:18:00 +0200

    00_20250603_OM: initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# A047_Vitesse  
+  
+**OpenClassroom training:**  
+Projet 8 - Mission - Créez une application Android en MVVM de A à Z  
+  
+[Link to the Openclassroom project](https://openclassrooms.com/fr/paths/527/projects/1640/364-mission---option-b---cas-fictif)  
+  
---
+++ app/src/main/java/com/example/vitesse/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.vitesse
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.vitesse.ui.theme.A047_VitesseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            A047_VitesseTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    A047_VitesseTheme {
+        Greeting("Android")
+    }
+}
