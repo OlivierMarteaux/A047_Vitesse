1d0dee75e80b396a6ff7b2aae26c53b4ba2c0c8f
Olivier Marteaux <olivier_marteaux@hotmail.com>
2025-06-06 16:23:43 +0200

    added dimensions and font resources. detail screen layout in progress.

--- app/src/main/java/com/example/vitesse/ui/applicantDetail/ApplicantDetailScreen.kt
+++ app/src/main/java/com/example/vitesse/ui/applicantDetail/ApplicantDetailScreen.kt
@@ -1,14 +1,17 @@
 package com.example.vitesse.ui.applicantDetail
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Email
@@ -16,12 +19,22 @@ import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.vitesse.R
 import com.example.vitesse.ui.AppViewModelProvider
@@ -40,32 +53,44 @@ fun ApplicantDetailScreen (
     viewModel: ApplicantDetailViewModel = viewModel(factory = AppViewModelProvider.Factory)
 ){
     Scaffold(
-        modifier = modifier,
+        modifier = modifier.fillMaxSize(),
         topBar = { ApplicantDetailTopAppBar() },
     ){ topAppBarPadding ->
         Column(
             modifier = modifier
                 .fillMaxSize()
-                .padding(topAppBarPadding)
+                .padding(topAppBarPadding),
+            horizontalAlignment = Alignment.CenterHorizontally
         ){
-            Image(painter = painterResource(R.drawable.ic_launcher_foreground), contentDescription = null)
+            Image(
+                painter = painterResource(R.drawable.martyna_siddeswara),
+                contentScale = ContentScale.Crop,
+                modifier = modifier.height(195.dp),
+                contentDescription = null)
             Row(
-                horizontalArrangement = Arrangement.SpaceAround
+                horizontalArrangement = Arrangement.Center
             ){
-                Column(){
-                    Icon(Icons.Filled.Call, contentDescription = null)
-                    Text(text = "text_exemple")
-                }
-                Column(){
-                    Icon(painter = painterResource(R.drawable.chat_24px), contentDescription = null)
-                    Text(text = "text_exemple")
-                }
-                Column(){
-                    Icon(Icons.Filled.Email, contentDescription = null)
-                    Text(text = "text_exemple")
-                }
+                ApplicantContactIcon(
+                    icon = IconResource.Vector(Icons.Filled.Star),
+                    text = stringResource(R.string.call),
+                    onClick = { /*TODO*/ }
+                )
+                ApplicantContactIcon(
+                    icon = IconResource.Paint(painterResource(R.drawable.chat_24px)),
+                    text = stringResource(R.string.sms),
+                    onClick = { /*TODO*/ }
+                )
+                ApplicantContactIcon(
+                    icon = IconResource.Vector(Icons.Filled.Email),
+                    text = stringResource(R.string.email),
+                    onClick = { /*TODO*/ }
+                )
             }
-            Card(){
+            Card(
+                modifier = modifier
+                    .padding(12.dp)
+                    .fillMaxWidth()
+            ){
                 Column(){
                     Text(text = "text_exemple")
                     Text(text = "text_exemple")
@@ -95,13 +120,98 @@ fun ApplicantDetailTopAppBar(
     modifier: Modifier = Modifier
 ){
     TopAppBar (
-        title = { Text(text = "Alice JOHNSON") },
-        modifier = modifier,
-        navigationIcon = { Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null) },
+        title = { Text(
+            text = "Alice JOHNSON",
+            style = MaterialTheme.typography.titleMedium
+        )},
+        modifier = modifier.padding(
+            top = dimensionResource(R.dimen.padding_medium),
+            bottom = dimensionResource(R.dimen.padding_medium),
+            start = dimensionResource(R.dimen.padding_small),
+            end = dimensionResource(R.dimen.padding_small)),
+        navigationIcon = { TopAppBarIconButton(
+            icon = Icons.AutoMirrored.Filled.ArrowBack,
+            onClick = { /*TODO*/ },
+            modifier = modifier
+        )},
         actions = {
-            Icon(Icons.Filled.Star, contentDescription = null)
-            Icon(Icons.Filled.Edit, contentDescription = null)
-            Icon(Icons.Filled.Delete, contentDescription = null)
+            TopAppBarIconButton(
+                icon = Icons.Filled.Star,
+                onClick = { /*TODO*/ },
+                modifier = modifier
+            )
+            TopAppBarIconButton(
+                icon = Icons.Filled.Edit,
+                onClick = { /*TODO*/ },
+                modifier = modifier
+            )
+            TopAppBarIconButton(
+                icon = Icons.Filled.Delete,
+                onClick = { /*TODO*/ },
+                modifier = modifier
+            )
         }
     )
+}
+
+@Composable
+fun ApplicantContactIcon(
+    icon: IconResource,
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.padding(12.dp)
+    ){
+        IconButton(
+            onClick = onClick,
+            modifier = modifier,
+        ){when (icon) {
+            is IconResource.Vector -> Icon(
+                imageVector = icon.imageVector,
+                contentDescription = null,
+                modifier = Modifier.border(
+                    width = Dp.Hairline,
+                    color = MaterialTheme.colorScheme.primary,
+                    shape = CircleShape
+                ).padding(8.dp)
+            )
+            is IconResource.Paint -> Icon(
+                painter = icon.painter,
+                contentDescription = null,
+                modifier = Modifier.border(
+                    width = Dp.Hairline,
+                    color = MaterialTheme.colorScheme.primary,
+                    shape = CircleShape
+                ).padding(8.dp)
+            )
+        }}
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodySmall
+        )
+    }
+}
+
+@Composable
+fun TopAppBarIconButton(
+    icon: ImageVector,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    IconButton(
+        onClick = onClick,
+        modifier = modifier,
+    ){Icon(
+        imageVector = icon,
+        contentDescription = null,
+        modifier = modifier.padding(dimensionResource(R.dimen.padding_medium))
+    )}
+}
+
+sealed class IconResource {
+    data class Vector(val imageVector: ImageVector) : IconResource()
+    data class Paint(val painter: Painter) : IconResource()
 }
